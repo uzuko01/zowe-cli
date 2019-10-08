c8050f49abdad4d728320ca5acede54a907730ad
Kostadin Uzunov <ku670569@broadcom.net>
2019-10-08 15:57:32 +0200

    add basic handler and definition

---
+++ packages/caspool/src/cli/CaSpool.definition.ts
@@ -0,0 +1,37 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+import { ICommandDefinition } from "@brightside/imperative";
+import { IssueCommand } from "./issue/Issue.definition";
+import { ZosmfSession } from "../../../zosmf";
+
+export const definition: ICommandDefinition = {
+    name: "ca-spool",
+    aliases: ["esf"],
+    type: "group",
+    summary: "Interact with CA Spool",
+    description: "Issue TSO commands and interact with Ca Spool",
+    children: [
+        IssueCommand
+    ],
+    passOn: [{
+        property: "options",
+        value: ZosmfSession.ZOSMF_CONNECTION_OPTIONS,
+        merge: true,
+        ignoreNodes: [
+            {
+                type: "group"
+            }
+        ]
+    }]
+};
+
+module.exports = definition;
---
+++ packages/caspool/src/cli/issue/command/Command.handler.ts
@@ -0,0 +1,43 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
+import { IHandlerParameters } from "@brightside/imperative";
+import { IIssueResponse, IssueTso } from "../../../../../zostso";
+import { ZosTsoBaseHandler } from "../../../../../zostso/src/ZosTsoBaseHandler";
+
+/**
+ * Handler to issue command to TSO address space
+ * @export
+ * @class Handler
+ * @implements {ICommandHandler}
+ */
+export default class Handler extends ZosTsoBaseHandler {
+
+    // Process the command and produce the TSO response
+    public async processCmd(params: IHandlerParameters) {
+
+        // Issue the TSO command
+        const cmd = "CALL 'PUBLIC.STRRA03.TL(TSOCESF)' '" + params.arguments.commandText + "'";
+        const response: IIssueResponse = await IssueTso.issueTsoCommand(
+            this.mSession,
+            params.arguments.account,
+            cmd,
+            this.mTsoStart);
+
+        // If requested, suppress the startup
+        if (!params.arguments.suppressStartupMessages) {
+            this.console.log(response.startResponse.messages);
+        }
+        this.console.log(response.commandResponse);
+        // Return as an object when using --response-format-json
+        this.data.setObj(response);
+    }
+}
